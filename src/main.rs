05866ee65e9f9abd2a550c82ffa16503834bdc3b
James Sargant <shorinbonsai@gmail.com>
2024-11-06 18:54:05 -0500

    response body

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -26,9 +21,23 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let response = match request_line.as_str() {
-        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    let tokens: Vec<&str> = request_line.split_whitespace().collect();
+    let response = match tokens[0] {
+        "GET" => {
+            if tokens[1] == "/" {
+                format!("HTTP/1.1 200 OK\r\n\r\n")
+            } else if &tokens[1][..6] == "/echo/" {
+                format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    tokens[1][6..].len(),
+                    &tokens[1][6..]
+                )
+            } else {
+                format!("HTTP/1.1 404 Not Found\r\n\r\n")
+            }
+        }
+        _ => format!("HTTP/1.1 404 Not Found\r\n\r\n"),
     };
+
     stream.write_all(response.as_bytes()).unwrap();
 }
